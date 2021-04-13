86dd45bd7a42f8ab9aa9e9d384a33afa448a59c9
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2021-04-13 12:16:03 +0530

    app inside add book container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
-  );
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Book from './containers/Book';
+export default class App extends Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return (
+      <div className="App">
+        <Book />
+      </div>
+    )
+  }
 }
-
-export default App;
