01f4346d6d546bef9173c1501499f49fd33403dc
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2021-04-22 11:07:59 +0530

    add router

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Books from './containers/Books';
 import CreateBook from './containers/CreateBook';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 export default class App extends Component {
   constructor(props){
     super(props);
   }
   render() {
     return (
+      <Router>
       <div className="App">
-        <Books />
-
-        <CreateBook />
+        <Switch>
+          <Route path="/" exact component={() => <Books />} />
+          <Route path="/create" exact component={() => <CreateBook />} />
+          <Route path="/edit/:id" exact component={() => <CreateBook />} />
+        </Switch>
       </div>
+      </Router>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {BrowserRouter as Router} from 'react-router-dom';
+import {createBrowserHistory} from 'history';
+
+export const history = createBrowserHistory({forceRefresh: true})
 ReactDOM.render(
   <React.StrictMode>
     <App />
